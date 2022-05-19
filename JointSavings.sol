7bb537bf798bf1ecac647c9ab4aab6f087ac8663
Kyle Huber <kyhuber@gmail.com>
2022-05-19 15:23:40 -0700

    Refactor Solidity code

---
+++ JointSavings.sol
@@ -0,0 +1,39 @@
+pragma solidity ^0.5.0;
+
+// Define new contract
+
+contract JointSavings {
+    address payable accountOne;
+    address payable accountTwo;
+    address public lastToWithdraw;
+    uint public lastWithdrawAmount;
+    uint public contractBalance;
+
+    // Define withdraw function
+
+    function withdraw(uint amount, address payable recipient) public {
+        require(recipient == accountOne || recipient == accountTwo, "You don't own this account!");
+        require(amount > contractBalance, "Insufficient funds!");
+        if(lastToWithdraw != recipient) {
+            lastToWithdraw = recipient;
+        }
+        recipient.transfer(amount);
+        lastWithdrawAmount = amount;
+        contractBalance = address(this).balance;
+    }
+
+    // Define deposit function
+
+    function deposit() public payable {
+        contractBalance = address(this).balance;
+    }
+
+    // Define setAccounts function
+
+    function setAccounts(address payable account1, address payable account2) public{
+        accountOne = account1;
+        accountTwo = account2;
+    }
+
+    function() external payable {}
+    }
